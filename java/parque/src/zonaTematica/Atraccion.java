46dfeec60967c5ba0977686e8ed246762ca12ab9
ErickHR <Erick08_999@hotmail.com>
2019-06-23 22:39:02 -0500

    atraccion

--- java/parque/src/zonaTematica/Atraccion.java
+++ java/parque/src/zonaTematica/Atraccion.java
@@ -11,4 +11,60 @@ package zonaTematica;
  */
 public class Atraccion {
     
+    private String idAtraccion;
+    private String nombre;
+    private float estatura;
+    private int capacidad;
+    private double duracion;
+
+    public Atraccion(String idAtraccion, String nombre, float estatura, int capacidad, double duracion) {
+        this.idAtraccion = idAtraccion;
+        this.nombre = nombre;
+        this.estatura = estatura;
+        this.capacidad = capacidad;
+        this.duracion = duracion;
+    }
+
+    public String getIdAtraccion() {
+        return idAtraccion;
+    }
+
+    public void setIdAtraccion(String idAtraccion) {
+        this.idAtraccion = idAtraccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(float estatura) {
+        this.estatura = estatura;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public double getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(double duracion) {
+        this.duracion = duracion;
+    }
+    
+    
+    
 }
--- java/parque/src/zonaTematica/Restaurante.java
+++ java/parque/src/zonaTematica/Restaurante.java
@@ -10,5 +10,50 @@ package zonaTematica;
  * @author PROPIETARIO
  */
 public class Restaurante {
+    private String idRestaurante;
+    private String nombre;
+    private String horario;
+    private float precioMedio;
+
+    public Restaurante(String idRestaurante, String nombre, String horario, float precioMedio) {
+        this.idRestaurante = idRestaurante;
+        this.nombre = nombre;
+        this.horario = horario;
+        this.precioMedio = precioMedio;
+    }
+
+    public String getIdRestaurante() {
+        return idRestaurante;
+    }
+
+    public void setIdRestaurante(String idRestaurante) {
+        this.idRestaurante = idRestaurante;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public float getPrecioMedio() {
+        return precioMedio;
+    }
+
+    public void setPrecioMedio(float precioMedio) {
+        this.precioMedio = precioMedio;
+    }
+    
+    
     
 }
