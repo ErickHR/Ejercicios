b425ac366f2b5fec834abeced96ef8e5efe15089
ErickHR <Erick08_999@hotmail.com>
2019-06-25 20:22:15 -0500

    lp1

---
+++ java/parque/src/zonaTematica/Pases/Pases.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zonaTematica.Pases;
+
+/**
+ *
+ * @author PROPIETARIO
+ */
+public class Pases {
+    private String fecha; 
+    private String hora;
+    private String aforo;
+
+    public Pases(String fecha, String hora, String aforo) {
+        this.fecha = fecha;
+        this.hora = hora;
+        this.aforo = aforo;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getAforo() {
+        return aforo;
+    }
+
+    public void setAforo(String aforo) {
+        this.aforo = aforo;
+    }
+    
+}
--- java/parque/src/zonaTematica/ZonaTematica.java
+++ java/parque/src/zonaTematica/ZonaTematica.java
@@ -55,6 +55,4 @@ public class ZonaTematica {
         this.color = color;
     }
     
-    
-    
 }
